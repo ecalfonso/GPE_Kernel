081704dd65655d8bbada274d647f4fd5ee32cc39
Steve Kondik <shade@chemlab.org>
2013-05-04 04:41:48 -0700

    Fix compilation issues with CONFIG_SEC_DEBUG disabled.
    Change-Id: I597ea4d521cc593e8ae1107710b2b412b456d9d1
    
    Conflicts:
    	arch/arm/mach-msm/pm-8x60.c
    	drivers/video/msm/mipi_dsi_host.c

--- drivers/video/msm/mipi_dsi_host.c
+++ drivers/video/msm/mipi_dsi_host.c
@@ -1691,7 +1691,9 @@ int mipi_dsi_cmd_dma_tx(struct dsi_buf *tp)
 		pr_err("%s: dma timeout error\n", __func__);
 		dumpreg(0);
 		dumstate(0);
+#ifdef CONFIG_SEC_DEBUG_MDP
 		sec_debug_mdp.dsi_err.mipi_tx_time_out_err_cnt++;
+#endif
 		mdp4_dump_regs();
 		dsi_clk_dump();
 		console_verbose();
@@ -1926,7 +1928,9 @@ void mipi_dsi_ack_err_status(void)
 
 	if (status) {
 		MIPI_OUTP(MIPI_DSI_BASE + 0x0064, status);
+#ifdef CONFIG_SEC_DEBUG_MDP
 		sec_debug_mdp.dsi_err.mipi_dsi_ack_err_status = status;
+#endif
 		/*
 		 * base on hw enginner, write an extra 0 needed
 		 * to clear error bits
@@ -1943,7 +1947,9 @@ void mipi_dsi_timeout_status(void)
 	status = MIPI_INP(MIPI_DSI_BASE + 0x00bc);/* DSI_TIMEOUT_STATUS */
 	if (status & 0x0111) {
 		MIPI_OUTP(MIPI_DSI_BASE + 0x00bc, status);
+#ifdef CONFIG_SEC_DEBUG_MDP
 		sec_debug_mdp.dsi_err.mipi_dsi_timeout_status = status;
+#endif
 		pr_debug("%s: status=%x\n", __func__, status);
 	}
 }
@@ -1956,7 +1962,9 @@ void mipi_dsi_dln0_phy_err(void)
 
 	if (status & 0x011111) {
 		MIPI_OUTP(MIPI_DSI_BASE + 0x00b0, status);
+#ifdef CONFIG_SEC_DEBUG_MDP
 		sec_debug_mdp.dsi_err.mipi_dsi_dln0_phy_err = status;
+#endif
 		pr_debug("%s: status=%x\n", __func__, status);
 	}
 }
@@ -1969,7 +1977,9 @@ void mipi_dsi_fifo_status(void)
 
 	if (status & 0x44444489) {
 		MIPI_OUTP(MIPI_DSI_BASE + 0x0008, status);
+#ifdef CONFIG_SEC_DEBUG_MDP
 		sec_debug_mdp.dsi_err.mipi_dsi_fifo_status = status;
+#endif
 		pr_err("%s: Error: status=%x\n", __func__, status);
 		mipi_dsi_sw_reset();
 		schedule_work(&mdp_reset_work);
@@ -1984,7 +1994,9 @@ void mipi_dsi_status(void)
 
 	if (status & 0x80000000) {
 		MIPI_OUTP(MIPI_DSI_BASE + 0x0004, status);
+#ifdef CONFIG_SEC_DEBUG_MDP
 		sec_debug_mdp.dsi_err.mipi_dsi_status = status;
+#endif
 		pr_debug("%s: status=%x\n", __func__, status);
 	}
 }
@@ -2021,7 +2033,9 @@ int mipi_runtime_clk_change(int fps)
 
 	if (!rc) {
 		pr_err("%s: dma timeout error\n", __func__);
+#ifdef CONFIG_SEC_DEBUG_MDP
 		sec_debug_mdp.dsi_err.fps_chage_time_out_err_cnt++;
+#endif
 	}
 		
 	mutex_unlock(&fps_done_mutex);
@@ -2058,7 +2072,9 @@ int mipi_runtime_csc_update(uint32_t reg[][2], int length)
 
 	if (!rc) {
 		pr_err("%s: dma timeout error\n", __func__);
+#ifdef CONFIG_SEC_DEBUG_MDP
 		sec_debug_mdp.dsi_err.fps_chage_time_out_err_cnt++;
+#endif
 	}
 		
 	mutex_unlock(&fps_done_mutex);
