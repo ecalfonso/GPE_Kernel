161dfd7655c00cfa8422acf6830d6b415120155d
Shubhraprakash Das <sadas@codeaurora.org>
2013-05-28 17:11:41 -0600

    msm: kgsl: Do not return an error on NULL gpu address
    If a NULL gpu address is passed to snapshot object tracking
    function then do not treat this as an error and return 0. NULL
    objects may be present in an IB so just skip over these objects
    instead of exiting due to an error.
    
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    Change-Id: Ic253722c58b41f41d03f83c77017e58365da01a7

--- drivers/gpu/msm/kgsl_snapshot.c
+++ drivers/gpu/msm/kgsl_snapshot.c
@@ -357,6 +357,9 @@ int kgsl_snapshot_get_object(struct kgsl_device *device, unsigned int ptbase,
 	if (!gpuaddr)
 		return 0;
 
+	if (!gpuaddr)
+		return 0;
+
 	entry = kgsl_get_mem_entry(device, ptbase, gpuaddr, size);
 
 	if (entry == NULL) {
